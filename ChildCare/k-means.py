f0bc1ad10b89e647ec21520c594afb0e861d3c3c
hsh6449 <57973170+hsh6449@users.noreply.github.com>
2021-08-14 16:47:01 +0900

    update

--- ChildCare/k-means.py
+++ ChildCare/k-means.py
@@ -48,10 +48,14 @@ plt.show
 
 #%% pickle data load
 import pickle
+from Spatial_Analysis import GdfCompare
+
 with open('gdf_6000_won', 'rb') as f :
-    a = pickle.load(f)
+    a = pickle.load(f)   
 with open('gdf_7000_won', 'rb') as f :
-    b = pickle.load(f2)
+    b = pickle.load(f)
+with open('법정동_지도_정보_추가', 'rb') as f:
+    c = pickle.load(f)
 #%% data 1 : 6000원
 from sklearn.preprocessing import scale
 from sklearn.datasets import load_iris
@@ -59,6 +63,8 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import stats
+from sklearn.metrics import silhouette_score ,silhouette_samples
 %matplotlib inline
 
 six_bf = a.gdf_before
@@ -77,6 +83,7 @@ six_bf_nc['target'] = six_bf.hotspot_class
 six_cluster_result = six_bf_nc.groupby(['target', 'cluster'])['mean_rest'].count()
 six_af_nc['target'] = six_af.hotspot_class
 six_cluster_result2 = six_af_nc.groupby(['target', 'cluster'])['mean_rest'].count()
+
 
 print(six_cluster_result)
 print(six_cluster_result2)
@@ -100,7 +107,7 @@ plt.scatter(x = six_bf_nc.loc[marker2_ind, 'pca_x'], y = six_bf_nc.loc[marker2_i
 plt.legend()
 plt.xlabel('PCA 1')
 plt.ylabel('PCA 2')
-plt.title('Clusters Visualization')
+plt.title('6000 Before policy: Clusters Visualization')
 plt.show()
 
 #%% visualization six_af_nc
@@ -121,7 +128,7 @@ plt.scatter(x = six_bf_nc.loc[marker2_ind, 'pca_x'], y = six_bf_nc.loc[marker2_i
 plt.legend()
 plt.xlabel('PCA 1')
 plt.ylabel('PCA 2')
-plt.title('Clusters Visualization')
+plt.title('6000 After policy:Clusters Visualization')
 plt.show()
 
 #%% 7000 k-means result
@@ -137,9 +144,9 @@ kmeans_4 = KMeans(n_clusters=3, init = 'k-means++', max_iter= 300, random_state 
 sev_af_nc['cluster'] = kmeans_4.labels_ 
 
 sev_bf_nc['target'] = sev_bf.hotspot_class
-sev_cluster_result = sev_bf_nc.groupby(['target', 'cluster'])['mean_rest'].count()
+sev_cluster_result = sev_bf_nc.groupby(['target', 'cluster'])['sum_school'].count()
 sev_af_nc['target'] = sev_af.hotspot_class
-sev_cluster_result2 = sev_af_nc.groupby(['target', 'cluster'])['mean_rest'].count()
+sev_cluster_result2 = sev_af_nc.groupby(['target', 'cluster'])['sum_school'].count()
 
 print(sev_cluster_result)
 print(sev_cluster_result2)
@@ -183,4 +190,334 @@ plt.xlabel('PCA 1')
 plt.ylabel('PCA 2')
 plt.title('Clusters Visualization')
 plt.show()
+#%% add 공시지가
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+
+merge = pd.merge(six_bf, c.loc[:,['ADM_DR_NM','공시지가','초등학생수','중학생수','고등학생수']], how ='inner', on= 'ADM_DR_NM')
+merge_2 = pd.merge(six_af, c.loc[:,['ADM_DR_NM','공시지가','초등학생수','중학생수','고등학생수']], how ='inner', on= 'ADM_DR_NM')
+
+six_bf_merge = merge[['sum_rest', 'sum_school', 'mean_rest','공시지가']]
+six_af_merge = merge_2[['sum_rest', 'sum_school', 'mean_rest','공시지가']]
+sc.fit(six_bf_merge)
+sc.fit(six_af_merge)
+
+kmeans = KMeans(n_clusters=3, init = 'k-means++', max_iter= 300, random_state = 40).fit(sc.transform(six_bf_merge))
+six_bf_merge['cluster'] = kmeans.labels_ 
+kmeans_2 = KMeans(n_clusters=3, init = 'k-means++', max_iter= 300, random_state = 40).fit(sc.transform(six_af_merge))
+six_af_merge['cluster'] = kmeans_2.labels_ 
+
+six_bf_merge['target'] = six_bf.hotspot_class
+six_merge_result = six_bf_merge.groupby(['target', 'cluster'])['sum_school'].count()
+six_af_merge['target'] = six_af.hotspot_class
+six_merge_result2 = six_af_merge.groupby(['target', 'cluster'])['sum_school'].count()
+
+
+six_bf_score = silhouette_score(six_bf_merge, kmeans.labels_,metric='euclidean')
+six_af_score = silhouette_score(six_af_merge, kmeans.labels_,metric='euclidean')
+
+print(six_merge_result)
+print(six_merge_result2)
+print('6000 before silhouette score: %.3f' %six_bf_score)
+print('6000 after silhouette score: %.3f' %six_af_score)
+
+#%% 시각화
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(six_bf_merge[['sum_rest', 'sum_school', 'mean_rest','공시지가']])
+
+six_bf_merge['pca_x'] = pca_transformed[:,0]
+six_bf_merge['pca_y'] = pca_transformed[:,1] 
+
+marker0_ind = six_bf_merge[six_bf_merge['cluster'] == 0].index
+marker1_ind = six_bf_merge[six_bf_merge['cluster'] == 1].index
+marker2_ind = six_bf_merge[six_bf_merge['cluster'] == 2].index
+
+plt.scatter(x = six_bf_merge.loc[marker0_ind, 'pca_x'], y = six_bf_merge.loc[marker0_ind, 'pca_y'], marker = 'o', label= 'very vulnerable')
+plt.scatter(x = six_bf_merge.loc[marker1_ind, 'pca_x'], y = six_bf_merge.loc[marker1_ind, 'pca_y'], marker = 's', label= 'even')
+plt.scatter(x = six_bf_merge.loc[marker2_ind, 'pca_x'], y = six_bf_merge.loc[marker2_ind, 'pca_y'], marker = '^', label= 'good')
+
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+
+print('공시지가 cor :',stats.pointbiserialr(x = six_bf_merge['공시지가'], y = six_bf_merge['cluster']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = six_bf_merge['sum_rest'], y = six_bf_merge['cluster']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = six_bf_merge['mean_rest'], y = six_bf_merge['cluster']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = six_bf_merge['sum_school'], y = six_bf_merge['cluster']))
+print("-----------------------------------------------------------------------------------------------------")
+print('공시지가 cor :',stats.pointbiserialr(x = six_bf_merge['공시지가'], y = six_bf_merge['target']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = six_bf_merge['sum_rest'], y = six_bf_merge['target']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = six_bf_merge['mean_rest'], y = six_bf_merge['target']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = six_bf_merge['sum_school'], y = six_bf_merge['target']))
+#%% 
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(six_af_merge[['sum_rest', 'sum_school', 'mean_rest','공시지가']])
+
+six_af_merge['pca_x'] = pca_transformed[:,0]
+six_af_merge['pca_y'] = pca_transformed[:,1] 
+
+marker0_ind = six_af_merge[six_af_merge['cluster'] == 0].index
+marker1_ind = six_af_merge[six_af_merge['cluster'] == 1].index
+marker2_ind = six_af_merge[six_af_merge['cluster'] == 2].index
+
+plt.scatter(x = six_af_merge.loc[marker0_ind, 'pca_x'], y = six_af_merge.loc[marker0_ind, 'pca_y'], marker = 'o', label= 'very vulnerable')
+plt.scatter(x = six_af_merge.loc[marker1_ind, 'pca_x'], y = six_af_merge.loc[marker1_ind, 'pca_y'], marker = 's', label= 'even')
+plt.scatter(x = six_af_merge.loc[marker2_ind, 'pca_x'], y = six_af_merge.loc[marker2_ind, 'pca_y'], marker = '^', label= 'good')
+
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+
+print('공시지가 cor :',stats.pointbiserialr(x = six_af_merge['공시지가'], y = six_af_merge['cluster']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = six_af_merge['sum_rest'], y = six_af_merge['cluster']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = six_af_merge['mean_rest'], y = six_af_merge['cluster']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = six_af_merge['sum_school'], y = six_af_merge['cluster']))
+print("-----------------------------------------------------------------------------------------------------")
+print('공시지가 cor :',stats.pointbiserialr(x = six_af_merge['공시지가'], y = six_af_merge['target']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = six_af_merge['sum_rest'], y = six_af_merge['target']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = six_af_merge['mean_rest'], y = six_af_merge['target']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = six_af_merge['sum_school'], y = six_af_merge['target']))
 #%%
+merge = pd.merge(sev_bf, c.loc[:,['ADM_DR_NM','공시지가','초등학생수','중학생수','고등학생수']], how ='inner', on= 'ADM_DR_NM')
+merge_2 = pd.merge(sev_af, c.loc[:,['ADM_DR_NM','공시지가','초등학생수','중학생수','고등학생수']], how ='inner', on= 'ADM_DR_NM')
+
+sev_bf_merge = merge[['sum_rest', 'sum_school', 'mean_rest','공시지가']]
+sev_af_merge = merge_2[['sum_rest', 'sum_school', 'mean_rest','공시지가']]
+
+sc.fit(sev_bf_merge)
+sc.fit(sev_af_merge)
+
+kmeans = KMeans(n_clusters=3, init = 'k-means++', max_iter= 300, random_state = 40).fit(sc.transform(sev_bf_merge))
+sev_bf_merge['cluster'] = kmeans.labels_ 
+kmeans_2 = KMeans(n_clusters=3, init = 'k-means++', max_iter= 300, random_state = 40).fit(sc.transform(sev_af_merge))
+sev_af_merge['cluster'] = kmeans_2.labels_ 
+
+sev_bf_merge['target'] = sev_bf.hotspot_class
+sev_merge_result = sev_bf_merge.groupby(['target', 'cluster'])['mean_rest'].count()
+sev_af_merge['target'] = sev_af.hotspot_class
+sev_merge_result2 = sev_af_merge.groupby(['target', 'cluster'])['mean_rest'].count()
+
+
+sev_bf_score = silhouette_score(sev_bf_merge, kmeans.labels_,metric='euclidean')
+sev_af_score = silhouette_score(sev_af_merge, kmeans.labels_,metric='euclidean')
+
+print(sev_merge_result)
+print(sev_merge_result2)
+print('7000 before silhouette score: %.3f' %sev_bf_score)
+print('7000 after silhouette score: %.3f' %sev_af_score)
+
+#%%
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(sev_bf_merge)
+
+sev_bf_merge['pca_x'] = pca_transformed[:,0]
+sev_bf_merge['pca_y'] = pca_transformed[:,1] 
+
+marker0_ind = sev_bf_merge[sev_bf_merge['cluster'] == 0].index
+marker1_ind = sev_bf_merge[sev_bf_merge['cluster'] == 1].index
+marker2_ind = sev_bf_merge[sev_bf_merge['cluster'] == 2].index
+
+plt.scatter(x = sev_bf_merge.loc[marker0_ind, 'pca_x'], y = sev_bf_merge.loc[marker0_ind, 'pca_y'], marker = 'o', label= 'very vulnerable')
+plt.scatter(x = sev_bf_merge.loc[marker1_ind, 'pca_x'], y = sev_bf_merge.loc[marker1_ind, 'pca_y'], marker = 's', label= 'even')
+plt.scatter(x = sev_bf_merge.loc[marker2_ind, 'pca_x'], y = sev_bf_merge.loc[marker2_ind, 'pca_y'], marker = '^', label= 'good')
+
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+
+print('공시지가 cor :',stats.pointbiserialr(x = sev_bf_merge['공시지가'], y = sev_bf_merge['cluster']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = sev_bf_merge['sum_rest'], y = sev_bf_merge['cluster']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = sev_bf_merge['mean_rest'], y = sev_bf_merge['cluster']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = sev_bf_merge['sum_school'], y = sev_bf_merge['cluster']))
+print("-----------------------------------------------------------------------------------------------------")
+print('공시지가 cor :',stats.pointbiserialr(x = sev_bf_merge['공시지가'], y = sev_bf_merge['target']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = sev_bf_merge['sum_rest'], y = sev_bf_merge['target']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = sev_bf_merge['mean_rest'], y = sev_bf_merge['target']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = sev_bf_merge['sum_school'], y = sev_bf_merge['target']))
+
+#%%
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(sev_af_merge)
+
+sev_af_merge['pca_x'] = pca_transformed[:,0]
+sev_af_merge['pca_y'] = pca_transformed[:,1] 
+
+marker0_ind = sev_af_merge[sev_af_merge['cluster'] == 0].index
+marker1_ind = sev_af_merge[sev_af_merge['cluster'] == 1].index
+marker2_ind = sev_af_merge[sev_af_merge['cluster'] == 2].index
+
+plt.scatter(x = sev_af_merge.loc[marker0_ind, 'pca_x'], y = sev_af_merge.loc[marker0_ind, 'pca_y'], marker = 'o', label= 'very vulnerable')
+plt.scatter(x = sev_af_merge.loc[marker1_ind, 'pca_x'], y = sev_af_merge.loc[marker1_ind, 'pca_y'], marker = 's', label= 'even')
+plt.scatter(x = sev_af_merge.loc[marker2_ind, 'pca_x'], y = sev_af_merge.loc[marker2_ind, 'pca_y'], marker = '^', label= 'good')
+
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+
+print('공시지가 cor :',stats.pointbiserialr(x = sev_af_merge['공시지가'], y = sev_af_merge['cluster']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = sev_af_merge['sum_rest'], y = sev_af_merge['cluster']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = sev_af_merge['mean_rest'], y = sev_af_merge['cluster']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = sev_af_merge['sum_school'], y = sev_af_merge['cluster']))
+print("-----------------------------------------------------------------------------------------------------")
+print('공시지가 cor :',stats.pointbiserialr(x = sev_af_merge['공시지가'], y = sev_af_merge['target']))
+print('식당 총 수 cor :',stats.pointbiserialr(x = sev_af_merge['sum_rest'], y = sev_af_merge['target']))
+print('식당수 평균 cor :',stats.pointbiserialr(x = sev_af_merge['mean_rest'], y = sev_af_merge['target']))
+print('학교 총 수 cor :',stats.pointbiserialr(x = sev_af_merge['sum_school'], y = sev_af_merge['target']))
+
+#%% same diff 시각화
+six_bf_merge['target'].loc[six_bf_merge['target'] == 1] = 2
+six_bf_merge['target'].loc[six_bf_merge['target'] == 0] = 1
+six_bf_merge['target'].loc[six_bf_merge['target'] == int(-1)] = 0
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(six_bf_merge[['sum_rest','sum_school', 'mean_rest','공시지가']])
+
+six_bf_merge['pca_x'] = pca_transformed[:,0]
+six_bf_merge['pca_y'] = pca_transformed[:,1] 
+
+diffidx = six_bf_merge[six_bf_merge['cluster'] != six_bf_merge['target']].index
+sameidx = six_bf_merge[six_bf_merge['cluster'] == six_bf_merge['target']].index
+total = (six_bf_merge['cluster'] != six_bf_merge['target']).sum() + (six_bf_merge['cluster'] == six_bf_merge['target']).sum()
+sm = (six_bf_merge['cluster'] == six_bf_merge['target']).sum()
+
+plt.scatter(x = six_bf_merge.loc[sameidx, 'pca_x'], y = six_bf_merge.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = six_bf_merge.loc[diffidx, 'pca_x'], y = six_bf_merge.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+plt.legend()
+plt.text(3, 30, sm/total)
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+#%%
+six_af_merge['target'].loc[six_af_merge['target'] == 1] = 2
+six_af_merge['target'].loc[six_af_merge['target'] == 0] = 1
+six_af_merge['target'].loc[six_af_merge['target'] == int(-1)] = 0
+
+
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(six_af_merge[['sum_rest','sum_school', 'mean_rest','공시지가']])
+
+six_af_merge['pca_x'] = pca_transformed[:,0]
+six_af_merge['pca_y'] = pca_transformed[:,1] 
+
+diffidx = six_af_merge[six_bf_merge['cluster'] != six_af_merge['target']].index
+sameidx = six_af_merge[six_bf_merge['cluster'] == six_af_merge['target']].index
+total = (six_af_merge['cluster'] != six_af_merge['target']).sum() + (six_af_merge['cluster'] == six_af_merge['target']).sum()
+sm = (six_af_merge['cluster'] == six_af_merge['target']).sum()
+
+plt.scatter(x = six_af_merge.loc[sameidx, 'pca_x'], y = six_af_merge.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = six_af_merge.loc[diffidx, 'pca_x'], y = six_af_merge.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+plt.text(3, 30, sm/total)
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+#%%
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == 1] = 2
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == 0] = 1
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == int(-1)] = 0
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(sev_bf_merge[['sum_rest','sum_school', 'mean_rest','공시지가']])
+
+sev_bf_merge['pca_x'] = pca_transformed[:,0]
+sev_bf_merge['pca_y'] = pca_transformed[:,1] 
+
+diffidx = sev_bf_merge[sev_bf_merge['cluster'] != sev_bf_merge['target']].index
+sameidx = sev_bf_merge[sev_bf_merge['cluster'] == sev_bf_merge['target']].index
+total = (sev_bf_merge['cluster'] != sev_bf_merge['target']).sum() + (sev_bf_merge['cluster'] == sev_bf_merge['target']).sum()
+sm = (sev_bf_merge['cluster'] == sev_bf_merge['target']).sum()
+
+plt.scatter(x = sev_bf_merge.loc[sameidx, 'pca_x'], y = sev_bf_merge.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = sev_bf_merge.loc[diffidx, 'pca_x'], y = sev_bf_merge.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+
+plt.text(3, 30, sm/total)
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+#%%
+sev_af_merge['target'].loc[sev_af_merge['target'] == 1] = 2
+sev_af_merge['target'].loc[sev_af_merge['target'] == 0] = 1
+sev_af_merge['target'].loc[sev_af_merge['target'] == int(-1)] = 0
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(sev_af_merge[['sum_rest','sum_school', 'mean_rest','공시지가']])
+
+sev_af_merge['pca_x'] = pca_transformed[:,0]
+sev_af_merge['pca_y'] = pca_transformed[:,1] 
+
+diffidx = sev_af_merge[sev_af_merge['cluster'] != sev_af_merge['target']].index
+sameidx = sev_af_merge[sev_af_merge['cluster'] == sev_af_merge['target']].index
+total = (sev_af_merge['cluster'] != sev_af_merge['target']).sum() + (sev_af_merge['cluster'] == sev_af_merge['target']).sum()
+sm = (sev_af_merge['cluster'] == sev_af_merge['target']).sum()
+
+plt.scatter(x = sev_af_merge.loc[sameidx, 'pca_x'], y = sev_af_merge.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = sev_af_merge.loc[diffidx, 'pca_x'], y = sev_af_merge.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+
+plt.text(3, 30, sm/total)
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+
+#%% 공시지가 없는 모델 시각화
+six_bf_nc['target'].loc[six_bf_nc['target'] == 1] = 2
+six_bf_nc['target'].loc[six_bf_nc['target'] == 0] = 1
+six_bf_nc['target'].loc[six_bf_nc['target'] == int(-1)] = 0
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(six_bf_nc[['sum_rest','sum_school', 'mean_rest']])
+
+six_bf_nc['pca_x'] = pca_transformed[:,0]
+six_bf_nc['pca_y'] = pca_transformed[:,1] 
+
+diffidx = six_bf_nc[six_bf_nc['cluster'] != six_bf_nc['target']].index
+sameidx = six_bf_nc[six_bf_merge['cluster'] == six_bf_nc['target']].index
+total = (six_bf_nc['cluster'] != six_bf_nc['target']).sum() + (six_bf_nc['cluster'] == six_bf_nc['target']).sum()
+sm = (six_bf_nc['cluster'] == six_bf_nc['target']).sum()
+
+plt.scatter(x = six_bf_nc.loc[sameidx, 'pca_x'], y = six_bf_nc.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = six_bf_nc.loc[diffidx, 'pca_x'], y = six_bf_nc.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+plt.legend()
+plt.text(30,5, sm/total)
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == 1] = 2
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == 0] = 1
+sev_bf_merge['target'].loc[sev_bf_merge['target'] == int(-1)] = 0
+
+
+
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(sev_bf_merge[['sum_rest','sum_school', 'mean_rest','공시지가']])
+
+sev_bf_merge['pca_x'] = pca_transformed[:,0]
+sev_bf_merge['pca_y'] = pca_transformed[:,1] 
+
+diffidx = sev_bf_merge[sev_bf_merge['cluster'] != sev_bf_merge['target']].index
+sameidx = sev_bf_merge[sev_bf_merge['cluster'] == sev_bf_merge['target']].index
+
+plt.scatter(x = sev_bf_merge.loc[sameidx, 'pca_x'], y = six_bf_merge.loc[sameidx, 'pca_y'], marker = 'o', label= 'same')
+plt.scatter(x = sev_bf_merge.loc[diffidx, 'pca_x'], y = six_bf_merge.loc[diffidx, 'pca_y'], marker = 's', label= 'Different')
+\
+
+plt.legend()
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('Clusters Visualization')
+plt.show()
